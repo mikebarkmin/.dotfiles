928e6243ede1bc69f5496a8eddea1cb5778c0a26
Mike Barkmin <mike.barkmin@uni-due.de>
2021-12-16 09:27:13 +0100

    automagic message

--- sway/.config/waybar/wf-recorder-status.sh
+++ sway/.config/waybar/wf-recorder-status.sh
@@ -3,5 +3,5 @@
 if pgrep -x wf-recorder >/dev/null; then
   echo '{"text":"On","class":"on","alt":"on"}'
 else
-  echo '{"text":"Off","class":"off","alt":"off"}'
+  echo '{"text":"Off","class":"off","alt":"off","tooltip": "Left: Screen\nRight: Window\nMiddle: Selection"}'
 fi
